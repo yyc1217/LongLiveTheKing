3a1c110820042aebbc218e8ae5370fd449b4628a
Yeh-Yung <yyc1217@gmail.com>
2014-06-10 00:17:58 +0800

    add hasNext() and next() to each game

--- schedule.js
+++ schedule.js
@@ -5,6 +5,16 @@ module.exports = (function() {
 	var _schedule = config.schedule,
 		that = {};
 	
+	_schedule.forEach(function(elem, index) {
+		elem.index = index;
+		elem.hasNext = function() {
+			return this.index < (Math.min(_schedule.length , 3) - 1);
+		};
+		elem.next = function() {
+			return _schedule[this.index+1];
+		};
+	});
+	
 	that.first = function() {
 			return _schedule[0];
 	};
