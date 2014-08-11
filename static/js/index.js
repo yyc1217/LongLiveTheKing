c0d900db19dcbb77d70273cb1fed3f087f2072e7
Yeh-Yung <yyc1217@gmail.com>
2014-08-11 10:01:41 +0800

    filtering result and run nodes for appending text elements

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,7 @@ var dictionary = {
 	rhino : '犀',
 	elephant : '象',
 	lion : '獅',
-	
+
 	win : '贏',
 	lose : '輸',
 	tie : '和'
@@ -33,29 +33,29 @@ var svg = d3.select("body").append("svg")
 
 var root;
 var i = 0;
-var LEVELS_OF_LAYER = 3;
+var LEVELS_OF_LAYER = 2;
 
 d3.json("result.json", function (error, json) {
 
-	root = json;	
+	root = json;
 	// first we need 'depth'
 	var nodes = tree.nodes(root);
-	
+
 	// decide subroot for each node
-	nodes.forEach(function(d) {
+	nodes.forEach(function (d) {
 		d.subroot = (d.depth % LEVELS_OF_LAYER == 0) ? d : d.parent.subroot;
 		d.id = ++i;
 	});
-	
+
 	update(1);
 });
 
 d3.select(self.frameElement).style("height", height + "px");
 
 function update(level) {
- 
+
 	var limit = level * LEVELS_OF_LAYER - 1;
- 
+
 	function cut(d) {
 		if (d.depth == limit) {
 			d._children = d.children;
@@ -71,13 +71,15 @@ function update(level) {
 	root.children.forEach(cut);
 
 	var nodes = tree.nodes(root).reverse(),
-		links = tree.links(nodes);
+	links = tree.links(nodes);
 
 	nodes.forEach(normalize);
 
 	var node = svg.selectAll("g.node")
-		.data(nodes, function(d){return d.id;});
-		
+		.data(nodes, function (d) {
+			return d.id;
+		});
+
 	var nodeEnter = node.enter()
 		.append("g")
 		.attr("class", function (d) {
@@ -86,13 +88,62 @@ function update(level) {
 		.attr('transform', function (d) {
 			return 'translate(' + d.subroot.y + ',' + d.subroot.x + ')';
 		});
-	
+
 	nodeEnter.append("circle")
-		.attr("r", 1e-6)
-		.attr('class', function (d) {
-			return !d.game ? d.winner : '';
+	.attr("r", 1e-6)
+	.attr('class', function (d) {
+		return !d.game ? d.winner : '';
+	});
+
+	var newRuns = nodeEnter.filter(function (d) {
+			return !d.game;
 		});
-		
+
+	newRuns.append('text')
+	.attr('dy', '.3em')
+	.attr('dx', '1em')
+	.attr('class', 'magic')
+	.text(function (d) {
+		if (d.tobeKing) {
+			if (d.parent && d.parent.parent && d.parent.parent.magicNumber == 0) {
+				return '';
+			}
+			if (d.magicNumber == 0) {
+				return dictionary[d.tobeKing] + '封王';
+			} else if (d.magicNumber == null) {
+				return '';
+			} else {
+				return dictionary[d.tobeKing] + ' M' + d.magicNumber;
+			}
+		}
+	});
+
+	newRuns.append('text')
+	.attr('dy', '.3em')
+	.attr('dx', '-1em')
+	.attr('text-anchor', 'end')
+	.text(function (d) {
+		return d.winner == 'tie' ? dictionary[d.winner] : dictionary[d.winner] + dictionary['win'];
+	});
+
+	var newResults = nodeEnter.filter(function (d) {
+			return d.game;
+		});
+
+	newResults.append('text')
+	.attr('dy', '-.8em')
+	.attr('dx', '-2.4em')
+	.text(function (d) {
+		return d.game ? d.game.date : '';
+	});
+
+	newResults.append('text')
+	.attr('dy', '1.3em')
+	.attr('dx', '-1.9em')
+	.text(function (d) {
+		return d.game ? dictionary[d.game.guest] + ' v.s. ' + dictionary[d.game.home] : '';
+	});
+
 	var nodeUpdate = node.transition()
 		.duration(duration)
 		.attr("transform", function (d) {
@@ -100,100 +151,61 @@ function update(level) {
 		});
 
 	nodeUpdate.select('circle')
-		.attr('r', 7);
-		
+	.attr('r', 7);
+
 	var nodeExit = node.exit()
 		.transition()
 		.duration(duration)
-		.attr("transform", function(d) { return "translate(" + d.subroot.y + "," + d.subroot.x + ")"; })
-		.remove();
-	
-	nodeExit.select('circle')
-		.attr('r', 1e-6);
-			
-	var link = svg.selectAll(".link")
-		.data(links, function(d){return d.target.id;});
-		
-	link.enter()
-		.insert('path', 'g')
-		.attr("class", "link")
-		.attr('d', function(d) {
-			var source = (d.target.subroot == d.target) ? d.source : d.target.subroot;
-			var o = {x: source.x, y: source.y};
-			return diagonal({source: o, target: o});
-		});
-
-	link.transition()
-		.duration(duration)
-		.attr("d", diagonal);
-	
-	link.exit()
-		.transition()
-		.duration(duration)
-		.attr('d', function(d) {
-			var source = (d.target.subroot == d.target) ? d.source : d.target.subroot;
-			var o = {x:source.x, y: source.y};
-			return diagonal({source: o, target: o});
+		.attr("transform", function (d) {
+			return "translate(" + d.subroot.y + "," + d.subroot.x + ")";
 		})
 		.remove();
-	
-	renderText();
+
+	nodeExit.select('circle')
+	.attr('r', 1e-6);
+
+	var link = svg.selectAll(".link")
+		.data(links, function (d) {
+			return d.target.id;
+		});
+
+	link.enter()
+	.insert('path', 'g')
+	.attr("class", "link")
+	.attr('d', function (d) {
+		var source = (d.target.subroot == d.target) ? d.source : d.target.subroot;
+		var o = {
+			x : source.x,
+			y : source.y
+		};
+		return diagonal({
+			source : o,
+			target : o
+		});
+	});
+
+	link.transition()
+	.duration(duration)
+	.attr("d", diagonal);
+
+	link.exit()
+	.transition()
+	.duration(duration)
+	.attr('d', function (d) {
+		var source = (d.target.subroot == d.target) ? d.source : d.target.subroot;
+		var o = {
+			x : source.x,
+			y : source.y
+		};
+		return diagonal({
+			source : o,
+			target : o
+		});
+	})
+	.remove();
+
 }
 
 function normalize(d) {
 	d.y = d.depth * 130;
-}
-
-function renderText() {
-
-	svg.selectAll('g.node.result')
-		.each(function (d) {
-		
-		d3.select(this)
-		.append('text')
-		.attr('dy', '.3em')
-		.attr('dx', '1em')
-		.attr('class', 'magic')
-		.text(function (d) {
-			if (d.tobeKing) {
-				if (d.parent && d.parent.parent && d.parent.parent.magicNumber == 0) {
-					return '';
-				}
-				if (d.magicNumber == 0) {
-					return dictionary[d.tobeKing] + '封王';
-				} else if (d.magicNumber == null) {
-					return '';
-				} else {
-					return dictionary[d.tobeKing] + ' M' + d.magicNumber;
-				}
-			}
-		});
-		d3.select(this)
-		.append('text')
-		.attr('dy', '.3em')
-		.attr('dx', '-1em')
-		.attr('text-anchor', 'end')
-		.text(function(d){
-			return d.winner == 'tie' ? dictionary[d.winner] : dictionary[d.winner] + dictionary['win'];
-		});
-	});
-	
-	svg.selectAll('g.node.run')
-		.each(function (d) {
-			d3.select(this)
-			.append('text')
-			.attr('dy', '-.8em')
-			.attr('dx', '-2.4em')
-			.text(function (d) {
-				return d.game ? d.game.date : '';
-			});
-
-			d3.select(this)
-			.append('text')
-			.attr('dy', '1.3em')
-			.attr('dx', '-1.9em')
-			.text(function (d) {
-				return d.game ? dictionary[d.game.guest] + ' v.s. ' + dictionary[d.game.home] : '';
-			});
-		});
 }
